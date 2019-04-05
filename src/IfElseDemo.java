66a96f7b863c297550478d7b178098ebf015db86
Justin <justinxwynkoop@gmail.com>
2019-04-05 09:46:44 -0400

    Extract Delegate Refactoring to create InsuranceStrategyVeryHigh

--- src/IfElseDemo.java
+++ src/IfElseDemo.java
@@ -1,29 +1,19 @@
 public class IfElseDemo {
 
+    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+
     public double calculateInsurance(double income) {
-    if (income <= 10000) {
-        return income*0.365;
-    } else if (income <= 30000) {
-        return (income-10000)*0.2+35600;
-    } else if (income <= getAdjustment()) {
-        return (income-30000)*0.1+76500;
-    } else {
-        return calculateInsuranceVeryHigh(income);
-    }
-}
-    public double calculateInsuranceVeryHigh(double income) {
-        return (income- getAdjustment())* getWeight() + getConstant();
-    }
 
-    private int getConstant() {
-        return 105600;
-    }
 
-    private double getWeight() {
-        return 0.02;
-    }
+        if (income <= 10000) {
+            return income * 0.365;
+        } else if (income <= 30000) {
+            return (income - 10000) * 0.2 + 35600;
+        } else if (income <= 60000) {
+            return (income - 30000) * 0.1 + 76500;
+        } else {
+            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+        }
 
-    private int getAdjustment() {
-        return 60000;
     }
 }
