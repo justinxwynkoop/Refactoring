3f6fc1dd36c2be4a78798f014ee889e9109c3db4
Justin <justinxwynkoop@gmail.com>
2019-04-05 09:49:52 -0400

    Fine Tuning Section

--- src/IfElseDemo.java
+++ src/IfElseDemo.java
@@ -1,19 +1,21 @@
 public class IfElseDemo {
 
-    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+    private InsuranceStrategyVeryHigh strategy;
 
     public double calculateInsurance(double income) {
 
 
         if (income <= 10000) {
-            return income * 0.365;
+            return income*0.365;
         } else if (income <= 30000) {
-            return (income - 10000) * 0.2 + 35600;
+            return (income-10000)*0.2+35600;
         } else if (income <= 60000) {
-            return (income - 30000) * 0.1 + 76500;
+            return (income-30000)*0.1+76500;
         } else {
-            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         }
 
     }
+
 }
