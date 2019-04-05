93591007621f477ebdc451b6413e113df6d11727
Justin <justinxwynkoop@gmail.com>
2019-04-05 09:41:38 -0400

    Invoked Extract Method Dialog for 6000, 0.02, 105600

--- src/IfElseDemo.java
+++ src/IfElseDemo.java
@@ -1,17 +1,29 @@
 public class IfElseDemo {
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            return income*0.365;
-        } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
-        } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
-        } else {
-            return calculateInsuranceVeryHigh(income);
-        }
+    if (income <= 10000) {
+        return income*0.365;
+    } else if (income <= 30000) {
+        return (income-10000)*0.2+35600;
+    } else if (income <= getAdjustment()) {
+        return (income-30000)*0.1+76500;
+    } else {
+        return calculateInsuranceVeryHigh(income);
     }
+}
     public double calculateInsuranceVeryHigh(double income) {
-        return (income-60000)*0.02+105600;
+        return (income- getAdjustment())* getWeight() + getConstant();
+    }
+
+    private int getConstant() {
+        return 105600;
+    }
+
+    private double getWeight() {
+        return 0.02;
+    }
+
+    private int getAdjustment() {
+        return 60000;
     }
 }
